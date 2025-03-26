a0e57cfd99003a38d4c7b1d6f6c43fc5cb16e9ab
brenaudon <brenaudon9@gmail.com>
2025-03-26 15:25:25 +0100

    Norme

--- 0_starting/ex09/setup.py
+++ 0_starting/ex09/setup.py
@@ -6,7 +6,8 @@ setup(
     description="A sample test package",
     author="brenaudo",
     author_email="brenaudo@student.42angouleme.fr",
-    url="https://github.com/brenaudon/python-for-data-science/tree/main/0_starting/ex09",
+    url="https://github.com/brenaudon/python-"
+        + "for-data-science/tree/main/0_starting/ex09",
     license="MIT",
     packages=find_packages(),  # automatically detects ft_package/
     python_requires=">=3.10",
